ee2581a464da522fed4d6801abbe71a1caa07a35
Gbenga Vincent Akinwande <akinwandegbengavincent@gmail.com>
2022-10-11 22:06:19 +0100

    added some error checking

--- index.js
+++ index.js
@@ -44,16 +44,21 @@ console.log("Initial output of root")
 console.log(output); //126a97e6963a8d86b53757446743db0d66e62dad392d29d2986a8a52348a2cea
 
 console.log("\n\nOutput of root when node 0 and 1 are updated and the new root is calculatedd and displayed")
-merkleTree.updateChild(1,'sffeefefegf')
-merkleTree.updateChild(0,'sffeeejefefefegf')
+merkleTree.updateChild(1,'sffeefefegf') //Replace the content of A
+merkleTree.updateChild(0,'sffeeejefefefegf') // Replace the content of B
 // merkleTree.updateChild(2,'sffeeeklwdjefefefegf')
 // merkleTree.updateChild(3,'sffeealskjnceklwdjefefefegf')
 output = merkleTree.getRoot();
 console.log(output); //8fb61de24708ddae9b89fc0f97f14ea6d88ec64ad879c229f44d7bcf2b5c609c
-hash = keccakDigestFunction('566219a21fa845dac884ad694f1e4861f7fb5f9216bc10d412c0adeddecd7f4633e406e29108896bad70e8e624cc088301e5fcc2925e6c4c865a108117e10ef5')
+hash = keccakDigestFunction(C)
 console.log("\n\nGetting the proof of inclusion of a node with hash, ", hash)
 included_node = merkleTree.getProofOfInclusion(hash)
-console.log(included_node.peakBagging.get_hash_content())
-console.log(included_node)
+if (Object.keys(included_node).length === 0){
+    console.log(included_node) // This helps prevent error when a node is node found
+}
+else{
+    console.log(included_node.peakBagging.get_hash_content())
+    console.log(included_node)
+}
 // verify 
 
