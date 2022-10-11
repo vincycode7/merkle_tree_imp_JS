914b26231c8ecb295ce72ab23aa98df7b2bf6237
Gbenga Vincent Akinwande <akinwandegbengavincent@gmail.com>
2022-10-11 19:00:02 +0100

    tested method for getproofinclusion

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ merkleTree.updateChild(0,'sffeeejefefefegf')
 // merkleTree.updateChild(2,'sffeeeklwdjefefefegf')
 // merkleTree.updateChild(3,'sffeealskjnceklwdjefefefegf')
 output = merkleTree.getRoot();
-console.log(output); //126a97e6963a8d86b53757446743db0d66e62dad392d29d2986a8a52348a2cea
+console.log(output); //8fb61de24708ddae9b89fc0f97f14ea6d88ec64ad879c229f44d7bcf2b5c609c
 hash = keccakDigestFunction('566219a21fa845dac884ad694f1e4861f7fb5f9216bc10d412c0adeddecd7f4633e406e29108896bad70e8e624cc088301e5fcc2925e6c4c865a108117e10ef5')
 console.log("\n\nGetting the proof of inclusion of a node with hash, ", hash)
 included_node = merkleTree.getProofOfInclusion(hash)
